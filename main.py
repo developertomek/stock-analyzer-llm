b7d03baa7dbcc6214076690344d4081f11afd77e
developertomek <tomekcodes@gmail.com>
2025-02-23 16:22:11 +0100

    Add static file serving and initial HTML, CSS, and JS files

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
+static_dir = os.path.join(os.path.dirname(__file__), "static")
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return FileResponse(os.path.join(static_dir, "index.html"))
+
