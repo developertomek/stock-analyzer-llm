0cdee72b1c05cd10c7d257f34de257747bcefc11
developertomek <tomekcodes@gmail.com>
2025-03-20 20:23:30 +0100

    Add HomeComponent with basic template and unit tests

---
+++ frontend/src/app/features/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  template: `<h1>Welcome to the Home Page</h1>`,
+})
+export class HomeComponent {
+
+}
